49ea1d04a1ee6ea8d1211c63e078907e27b0f17c
qscv06532 <qscv06532@gmail.com>
2025-12-04 09:08:55 +0900

    Add files via upload

---
+++ AlgorithmStudy/5주차/BOJ1920/이은희1920.java
@@ -0,0 +1,39 @@
+package algorithm.study.day05;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class FindSu {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		Set<Integer> list = new HashSet<>();
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		for(int i = 0; i < n; i++) {
+			list.add(Integer.parseInt(st.nextToken()));
+		}
+		
+		int m = Integer.parseInt(br.readLine());
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		
+		for(int i = 0; i < m; i++) {
+			int find = Integer.parseInt(st.nextToken());
+			sb.append(list.contains(find)?1:0);
+			if(i != m-1) {
+				sb.append("\n");
+			}
+		}
+		System.out.println(sb);
+	}
+
+}

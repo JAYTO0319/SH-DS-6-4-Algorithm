82b0a3d9e9488942066658a311d9f8c5a50e2dca
KIM YONUGBEEN <81636454+sttarrynight@users.noreply.github.com>
2025-12-08 15:10:38 +0900

    5주차 문제 풀이 업로드 2750번

---
+++ AlgorithmStudy/5주차/BOJ2750/김영빈2750.java
@@ -0,0 +1,32 @@
+package algorithm.week6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class 김영빈2750 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		List<Integer> list = new ArrayList<>();
+		
+		while(N-->0) {
+			int input = Integer.parseInt(br.readLine());
+			list.add(input);
+		}
+		
+		list.sort((x,y)->x-y);
+		
+		for(int i : list) {
+			sb.append(i).append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+}

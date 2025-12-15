a97c8dfe7f74cdf85ee7133de48f4eb5dac638c0
김아현 <kimah0403@gmail.com>
2025-12-15 16:34:25 +0900

    김아현11047

---
+++ AlgorithmStudy/6주차/BOJ11047/김아현11047.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 김아현11047 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        int N = Integer.parseInt(stringTokenizer.nextToken());
+        int K = Integer.parseInt(stringTokenizer.nextToken());
+        int[] A = new int[N];
+        int count = 0;
+
+        for(int i = 0; i < N; i++){
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            A[i] = Integer.parseInt(stringTokenizer.nextToken());
+        }
+
+        for(int i = N-1; i >= 0; i--){
+            int result = 0;
+            if(K / A[i] >= 1){
+                result= K / A[i];
+                count += result;
+                K = K - result * A[i];
+            }
+        }
+        System.out.println(count);
+    }
+}

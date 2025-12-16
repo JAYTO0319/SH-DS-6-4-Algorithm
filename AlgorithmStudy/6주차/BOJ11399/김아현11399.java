00cd342b92d7ce77c8cfb3b8675e35381a461116
김아현 <kimah0403@gmail.com>
2025-12-16 14:48:34 +0900

    김아현11399

---
+++ AlgorithmStudy/6주차/BOJ11399/김아현11399.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 김아현11399 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stringTokenizer;
+		
+		int N = Integer.parseInt(bufferedReader.readLine());
+		int[] arr = new int[N];
+		int sum = 0; int result = 0;
+		stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(stringTokenizer.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		for(int i = 0; i < arr.length; i++) {
+			sum += arr[i];
+			result += sum;
+		}
+		
+		System.out.println(result);
+	}
+}
+
+

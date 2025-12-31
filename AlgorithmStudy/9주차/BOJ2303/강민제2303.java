7aaaabb9cd5956e41eb8ae409b0a542ec5aa1ceb
Minje Kang <kangminje00@gmail.com>
2026-01-01 02:29:04 +0900

    Add logic to calculate maximum sum from three numbers
    Implemented logic to find the maximum sum of three numbers from input arrays and track the index of the corresponding array.

--- AlgorithmStudy/9주차/BOJ2303/강민제2303.java
+++ AlgorithmStudy/9주차/BOJ2303/강민제2303.java
@@ -2,6 +2,30 @@ public class Main2303 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[5];
+		int max=0, maxN=0, i=0;
+		for(i=0;i<N;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j=0;j<5;j++) arr[j] = Integer.parseInt(st.nextToken());
+			for(int j=0;j<3;j++) {
+				for(int k=j+1;k<4;k++) {
+					for(int l=k+1;l<5;l++) {
+						int sum = 0;
+						sum += arr[j] + arr[k] + arr[l];
+						if(sum%10 >= max) {
+							max = sum%10;
+							maxN = i;
+						}
+					}
+				}
+			}
+		}
+		System.out.println(maxN+1);
+	}
+	
+	public static void main2(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
 		int[][] arr = new int[N][5];
 		for(int i=0;i<N;i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());

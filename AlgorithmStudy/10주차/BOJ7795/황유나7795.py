f51f8285bd39d3f11a491a0f131654511630013e
Yuna Hwang <82206168+hyn4008@users.noreply.github.com>
2026-01-12 23:10:51 +0900

    Update 황유나7795.py

--- AlgorithmStudy/10주차/BOJ7795/황유나7795.py
+++ AlgorithmStudy/10주차/BOJ7795/황유나7795.py
@@ -5,6 +5,7 @@ input = sys.stdin.readline
 t = int(input())
 for _ in range(t):
     n, m = map(int, input().split())
+    alist = list(map(int, input().split()))
     blist = sorted(list(map(int, input().split())))
 
     total = 0

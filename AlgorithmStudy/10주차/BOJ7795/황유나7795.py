a541df168a5fc7e46092c2914144246cf3fb8807
Yuna Hwang <82206168+hyn4008@users.noreply.github.com>
2026-01-12 23:09:16 +0900

    Update 황유나7795.py

--- AlgorithmStudy/10주차/BOJ7795/황유나7795.py
+++ AlgorithmStudy/10주차/BOJ7795/황유나7795.py
@@ -5,10 +5,9 @@ input = sys.stdin.readline
 t = int(input())
 for _ in range(t):
     n, m = map(int, input().split())
-    alist = sorted(list(map(int, input().split())))
     blist = sorted(list(map(int, input().split())))
 
     total = 0
     for a in alist:
-        total += bisect_left(blist, a)
+        total += bisect_left(blist, a) # 정렬된 blist에서 순서상 a가 들어갈 수 있는 가장 왼쪽 인덱스 반환
     print(total)

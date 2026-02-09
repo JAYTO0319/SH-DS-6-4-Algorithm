d3eac7dea14d76bf549ce963366df532cab9c548
Minje Kang <kangminje00@gmail.com>
2026-02-09 17:47:26 +0900

    Create 강민제11866.java

---
+++ AlgorithmStudy/14주차/BOJ11866/강민제11866.java
@@ -0,0 +1,27 @@
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		Queue<Integer> q = new ArrayDeque<>();
+		for(int i=1;i<=N;i++) {
+			q.add(i);
+		}
+		StringBuilder sb = new StringBuilder();
+		sb.append("<");
+		for(int i=1;i<K;i++) {
+			q.add(q.poll());
+		}
+		sb.append(q.poll());
+		while(!q.isEmpty()) {
+			for(int i=1;i<K;i++) {
+				q.add(q.poll());
+			}
+			sb.append(", "+q.poll());
+		}
+		sb.append(">");
+		System.out.println(sb);
+	}
+}
